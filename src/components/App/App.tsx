c1489b1437f741e1467c36f93002d0a48010e43f
Bastien Pruvost <bastien.mory@gmail.com>
2023-04-04 20:34:12 +0200

    fix: remove eslint error in App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,6 @@
 import styles from './App.module.scss';
 
 export function App() {
-  const myConst = 'myConst';
-  if (true) {
-    console.log(myConst);
-  }
   return (
     <div className={styles.App}>
       <p>Hey hey</p>
